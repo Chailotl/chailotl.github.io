bb7a8aa73ad9f8b050b3ce42d05e0491f70fabf1
Chai <7232280+Chailotl@users.noreply.github.com>
2021-12-22 21:19:06 -0500

    Switched to apostrophe strings

--- js/countdown.js
+++ js/countdown.js
@@ -1,32 +1,32 @@
 // Read url parameters
 var urlParams = new URLSearchParams(window.location.search);
-var hour = urlParams.get("hour");
-var minute = urlParams.get("minute");
-var text = urlParams.get("text");
+var hour = urlParams.get('hour');
+var minute = urlParams.get('minute');
+var text = urlParams.get('text');
 
 // Check if we should show timer or ask for one
 if (hour === null) {
-	document.getElementById("form").style.display = "initial";
-	document.getElementById("timer").style.display = "none";
+	document.getElementById('form').style.display = 'initial';
+	document.getElementById('timer').style.display = 'none';
 }
 
 // Set default values
 if (minute === null) { minute = 0; }
-if (text === null) { text = "LIVE" }
+if (text === null) { text = 'LIVE' }
 
 // Get inputs, verify, and send user to url
 function openURL() {
-	var time = document.getElementById("time").value;
-	if (time == "") {
-		alert("Please input a time");
+	var time = document.getElementById('time').value;
+	if (time == '') {
+		alert('Please input a time');
 	} else {
-		time = time.split(":");
-		window.location = `https://chailotl.github.io/countdown?hour=${time[0]}&minute=${time[1]}&text=${document.getElementById("text").value}`;
+		time = time.split(':');
+		window.location = `https://chailotl.github.io/countdown?hour=${time[0]}&minute=${time[1]}&text=${document.getElementById('text').value}`;
 	}
 }
 
 function pad(num) {
-	return ("0" + parseInt(num)).substr(-2);
+	return ('0' + parseInt(num)).substr(-2);
 }
 
 // Set the date we're counting down to
@@ -44,15 +44,15 @@ var x = setInterval(function() {
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	// Output the result
-	var str = minutes + ":" + pad(seconds);
+	var str = minutes + ':' + pad(seconds);
 	if (hours > 0) {
-		str = hours + ":" + pad(minutes) + ":" + pad(seconds);
+		str = hours + ':' + pad(minutes) + ':' + pad(seconds);
 	}
-	document.getElementById("timer").innerHTML = str;
+	document.getElementById('timer').innerHTML = str;
 
 	// If the count down is over, write some text 
 	if (distance < 0) {
 		clearInterval(x);
-		document.getElementById("timer").innerHTML = text;
+		document.getElementById('timer').innerHTML = text;
 	}
 }, 100);
