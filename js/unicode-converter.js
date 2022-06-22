43fe4f7a9d9f526f4a6236f7da4dc667c9918474
Chai <7232280+Chailotl@users.noreply.github.com>
2022-06-22 11:53:12 -0400

    Create unicode-converter.js

---
+++ js/unicode-converter.js
@@ -0,0 +1,16 @@
+// Select all elements
+var input = document.getElementById('input')
+var outputs = []
+
+['circled']
+.forEach((item) =>
+{
+	outputs[item] = document.getElementById(item)
+})
+
+function update() {
+	outputs.forEach((item) =>
+	{
+		item.value = input.value
+	})
+}
