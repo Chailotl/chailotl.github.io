c90898b930200c8cc7cbde0100ddd778e3ebfe9e
Chai <7232280+Chailotl@users.noreply.github.com>
2022-06-24 15:24:42 -0400

    Update discord-embed.js

--- js/discord-embed.js
+++ js/discord-embed.js
@@ -1,3 +1,30 @@
+// Select elements
+var author = document.getElementById('author text')
+var title = document.getElementById('title text')
+var description = document.getElementById('description')
+var imageURL = document.getElementById('image url')
+var redirectURL = document.getElementById('redirect url')
+
 function copy () {
+	var str = 'https://chailotl.github.io/embed/?'
 	
+	if (author.value) {
+		str += 'author=' + author.value + '&'
+	}
+	if (title.value) {
+		str += 'title=' + title.value + '&'
+	}
+	if (description.value) {
+		str += 'desc=' + description.value + '&'
+	}
+	if (imageURL.value) {
+		str += 'image=' + imageURL.value + '&'
+	}
+	if (redirectURL.value) {
+		str += 'redirect=' + redirectURL.value + '&'
+	}
+	
+	str = str.substring(0, str.length - 1)
+	
+	navigator.clipboard.writeText(str)
 }
