e2b10701b9f6cd22b411b22df1898b5317ff7ca7
Chai <7232280+Chailotl@users.noreply.github.com>
2022-06-27 14:35:45 -0400

    Update spray.js

--- js/spray.js
+++ js/spray.js
@@ -4,13 +4,14 @@ function test() {
 
 // Import modules
 //import { saveAs } from "https://cdn.skypack.dev/save-as@0.1.8";
-import { Vtf, VtfImageResource, Frame } from 'https://cdn.jsdelivr.net/gh/koerismo/vtf.js/dist/module/VtfContainer.js';
+//import { Vtf, VtfImageResource, Frame } from 'https://cdn.jsdelivr.net/gh/koerismo/vtf.js/dist/module/VtfContainer.js';
 
 // Select all elements
 var input = document.getElementById('input')
 
 function saveSpray() {
-	var image = new Image()
+	alert('saveSpray')
+	/*var image = new Image()
 	var fr = new FileReader()
 	
 	fr.onload = () => { image.src = fr.result }
@@ -19,8 +20,6 @@ function saveSpray() {
 	var resource = new VtfImageResource([new Frame(image)])
 	
 	var vtf = new Vtf([img.width, img.height], [resource], 'DXT5')
-	window.saveAs(vtf.blob(), 'spray.vtf')
+	window.saveAs(vtf.blob(), 'spray.vtf')*/
 	//saveAs( vtf.blob(), 'spray.vtf')
 }
-
-input.addEventListener("oninput", saveSpray)
