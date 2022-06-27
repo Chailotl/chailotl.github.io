59886b57d3974c9bd2334b64a77ed329b703e4cb
Chai <7232280+Chailotl@users.noreply.github.com>
2022-06-27 13:53:57 -0400

    Create spray.js

---
+++ js/spray.js
@@ -0,0 +1,21 @@
+// Import modules
+import { saveAs } from "https://cdn.skypack.dev/save-as@0.1.8";
+import { Vtf, VtfImageResource, Frame } from 'https://cdn.jsdelivr.net/gh/koerismo/vtf.js@latest/vtf.js';
+
+// Select all elements
+var input = document.getElementById('input')
+
+var image = new Image();
+var fr = new FileReader()
+fr.onload = () => { image.src = fr.result }
+
+function save() {
+	fr.readAsDataURL(input.files[0])
+	
+	var resource = new VtfImageResource([
+		new Frame( image )
+	])
+	
+	var vtf = new Vtf([img.width, img.height], [resource], 'DXT5')
+	saveAs( vtf.blob(), 'spray.vtf')
+}
