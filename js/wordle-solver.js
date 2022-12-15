f8dd78a41491ecad610b89e72846e7c414ce4363
Chai <7232280+Chailotl@users.noreply.github.com>
2022-12-15 14:52:51 -0500

    Update wordle-solver.js

--- js/wordle-solver.js
+++ js/wordle-solver.js
@@ -2,18 +2,21 @@ var include = document.getElementById('include')
 var exclude = document.getElementById('exclude')
 var output = document.getElementById('output')
 
-var wordles = [
-	'apple',
-	'night',
-	'place',
-	'farce',
-	'shame'
-]
+var wordles = []
+
+fetch('/js/words.txt')
+	.then(response => response.text())
+	.then(text => wordles = text.split('\n'))
 
 function update() {
+	if (include.value == '' && exclude.value == '') {
+		output.innerText = ''
+		return
+	}
+	
 	var words = wordles.filter(word => {
 		var value = true;
-		[...include.value].forEach(char => {
+		[...include.value.toLowerCase()].forEach(char => {
 			if (word.indexOf(char) == -1) {
 				value = false
 				return
@@ -23,7 +26,7 @@ function update() {
 	})
 		.filter(word => {
 			var value = true;
-			[...exclude.value].forEach(char => {
+			[...exclude.value.toLowerCase()].forEach(char => {
 				if (word.indexOf(char) >= 0) {
 					value = false
 					return
