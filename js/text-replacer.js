2d592f585f2af35f76e6367011de3bc4810487ec
Chai <7232280+Chailotl@users.noreply.github.com>
2024-02-05 16:14:10 -0500

    Update text-replacer.js

--- js/text-replacer.js
+++ js/text-replacer.js
@@ -1,8 +1,7 @@
 // Select all elements
 const input = document.getElementById('input')
 const output = document.getElementById('output')
-const find = document.getElementById('find')
-const replace = document.getElementById('replace')
+let replacers = document.getElementsByClassName('text-replacer')
 
 function copy() {
 	output.select()
@@ -13,7 +12,13 @@ function copy() {
 function update() {
 	let text = input.value;
 
-	text = text.replaceAll(find.value, replace.value)
+	for (let replacer of replacers) {
+		let find = replacer.getElementsByClassName('find')[0]
+		let replace = replacer.getElementsByClassName('replace')[0]
+		let regex = replacer.getElementsByClassName('regex')[0]
+		
+		text = text.replaceAll(find.value, replace.value)
+	}
 	
 	output.value = text
 }
